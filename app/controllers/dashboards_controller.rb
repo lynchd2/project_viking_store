477a9dde953a2b12afa303388a28b4225b37faf2
essian <jessicampdennis@hotmail.com>
2016-08-03 18:23:43 +0100

    starts table

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,19 @@
 class DashboardsController < ApplicationController
 
   def index
-    @user_count = User.count
-    @order_count = Order.count
-    @orders_submitted=Order.select('count(checkout_date)')
-    @products = Product.count
-    @revenue = 
+    @user_count = User.where('created_at > ?',7.days.ago).count
+    @order_count = Order.where('checkout_date > ?',7.days.ago).count
+    @new_products = Product.where('created_at > ?',7.days.ago).count
+    @revenue = Product.find_by_sql("
+      SELECT (quantity * price) as order_total
+      from order_contents oc 
+      INNER JOIN products p on order_id = oc.id
+      limit 1
+
+      ")
+     
 
 
   end
 end
+
